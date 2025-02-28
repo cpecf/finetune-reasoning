86323c7faca136a609f56bcc22de2a0e33f277f1
cpecf <mina@bvm.network>
2025-02-28 08:00:33 +0000

    add files

---
+++ train.py
@@ -0,0 +1,77 @@
+from unsloth import FastLanguageModel, PatchFastRL
+PatchFastRL("GRPO", FastLanguageModel)
+from unsloth import is_bfloat16_supported
+import torch
+import re
+from datasets import load_dataset, Dataset
+from utils import xmlcount_reward_func,soft_format_reward_func,strict_format_reward_func,int_reward_func,correctness_reward_func,get_gsm8k_questions
+from dotenv import load_dotenv
+import os
+from trl import GRPOConfig, GRPOTrainer
+
+load_dotenv()
+
+max_seq_length = 512 # Can increase for longer reasoning traces
+lora_rank = 16 # Larger rank = smarter, but slower
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "unsloth/Phi-4",
+    max_seq_length = max_seq_length,
+    load_in_4bit = True, # False for LoRA 16bit
+    fast_inference = True, # Enable vLLM fast inference
+    max_lora_rank = lora_rank,
+    gpu_memory_utilization = 0.7, # Reduce if out of memory
+)
+
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = lora_rank, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+    target_modules = ["gate_proj", "up_proj", "down_proj",],
+    lora_alpha = lora_rank,
+    use_gradient_checkpointing = "unsloth", # Enable long context finetuning
+    random_state = 3407,
+)
+
+dataset = get_gsm8k_questions()
+
+training_args = GRPOConfig(
+    use_vllm = True, # use vLLM for fast inference!
+    learning_rate = 5e-6,
+    adam_beta1 = 0.9,
+    adam_beta2 = 0.99,
+    weight_decay = 0.1,
+    warmup_ratio = 0.1,
+    lr_scheduler_type = "cosine",
+    optim = "paged_adamw_8bit",
+    logging_steps = 1,
+    bf16 = is_bfloat16_supported(),
+    fp16 = not is_bfloat16_supported(),
+    per_device_train_batch_size = 1,
+    gradient_accumulation_steps = 1, # Increase to 4 for smoother training
+    num_generations = 6, # Decrease if out of memory
+    max_prompt_length = 256,
+    max_completion_length = 200,
+    # num_train_epochs = 1, # Set to 1 for a full training run
+    max_steps = 100,
+    save_steps = 250,
+    max_grad_norm = 0.1,
+    report_to = "none", # Can use Weights & Biases
+    output_dir = "outputs",
+)
+trainer = GRPOTrainer(
+    model = model,
+    processing_class = tokenizer,
+    reward_funcs = [
+        xmlcount_reward_func,
+        soft_format_reward_func,
+        strict_format_reward_func,
+        int_reward_func,
+        correctness_reward_func,
+    ],
+    args = training_args,
+    train_dataset = dataset,
+)
+trainer.train()
+
+model.save_pretrained_merged("phi-4-14b", tokenizer, save_method = "lora",)
+model.push_to_hub_merged("phi-4-14b", tokenizer, save_method = "lora", token = os.getenv("HF_TOKEN"))
